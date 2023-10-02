f1cfe4babbc775295ac81fa3fe966d7423d69113
Tino Bokoshev <bokoshevtino@gmail.com>
2023-10-02 16:11:09 +0200

    fix esercizio su array multidimensionali

--- projects/java-basics-01/09-arrays/Array4.java
+++ projects/java-basics-01/09-arrays/Array4.java
@@ -5,15 +5,14 @@ public class Array4 {
     public static int matrixSum(int[][] myMatrix) {
         int sum = 0;
         for (int i = 0; i < myMatrix.length; i++) {
-            for (int j = 0; j < myMatrix[i].length; j++) {
-                sum += myMatrix[i][j];
-            }
+            //aggiungo a sum i primi elementi di ogni array
+            sum += myMatrix[i][0];
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        int[][] testMatrix = { {1, 2, 3, 4}, {5, 6, 7} };
+        int[][] testMatrix = { {1, 2, 3, 4}, {5, 6, 7}, {8,9} };
         System.out.println(matrixSum(testMatrix));
     }
 }
